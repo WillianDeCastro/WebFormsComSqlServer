cc06566198fceb9e8d046093ab4653045f111e1e
WillianDeCastro <oilha2010@hotmail.com>
2018-08-28 23:10:58 -0300

    subida

---
+++ Publicacao/About.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+public partial class About : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+
+    protected void btnInfo_Click(object sender, EventArgs e)
+    {
+        string conecString = ConfigurationManager.ConnectionStrings["banco"].ConnectionString;
+
+
+        SqlConnection cn = new SqlConnection(conecString);
+
+        cn.Open();
+
+        DataTable dt = new DataTable();
+        dt.Load(new SqlCommand("Select Nome from Pessoas", cn).ExecuteReader());
+
+        cn.Close();
+    }
+}
---
+++ Publicacao/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Publicacao.Startup))]
+namespace Publicacao
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
